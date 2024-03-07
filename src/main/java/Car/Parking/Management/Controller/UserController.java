f196d6928802921bc97938b2ab99395d60cd8bcd
Pandi92 <pandiyarajang92@gmail.com>
2024-03-07 17:29:00 +0530

    backend

--- src/main/java/Car/Parking/Management/Controller/UserController.java
+++ src/main/java/Car/Parking/Management/Controller/UserController.java
@@ -3,6 +3,7 @@ package Car.Parking.Management.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,8 +16,9 @@ import Car.Parking.Management.Service.UserService;
 
 @RestController
 @RequestMapping("/user")
-public class UserController {
+@CrossOrigin(origins = "http://localhost:3000") 
 
+public class UserController {
 	@Autowired
 	private UserService userservice;
 	
